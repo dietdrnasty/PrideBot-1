73067fdfdaed17fe4b7e208a152cb4371561e47a
dietdrnasty <123226255+dietdrnasty@users.noreply.github.com>
2023-01-20 21:12:46 -0800

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from dotenv.main import load_dotenv
 load_dotenv()
 
 # Discord config
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "")
-BOT_PREFIX = "?"
+DISCORD_TOKEN = os.getenv(MTA2NTc3OTY5MDcxNzA2OTQ5Mw.GmMrIB.ngqMqvXz0eF_IpsMP_sWvQGQwdwj6J8q8b6W_Q, "")
+BOT_PREFIX = "<3"
